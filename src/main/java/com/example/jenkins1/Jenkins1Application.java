df85c8fb35433e3268f4d2cf45caffc5c2817488
Pratik-Kore <pratikkore777@gmail.com>
2024-03-19 14:38:50 +0530

    First commit on jenkins1

---
+++ src/main/java/com/example/jenkins1/Jenkins1Application.java
@@ -0,0 +1,24 @@
+package com.example.jenkins1;
+
+import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jenkins1Application {
+
+	public static Logger logger = LoggerFactory.getLogger(Jenkins1Application.class);
+
+	@PostConstruct
+	public void initt(){
+		logger.info("Application running.....");
+	}
+
+	public static void main(String[] args) {
+		logger.info("Application executed....");
+		SpringApplication.run(Jenkins1Application.class, args);
+	}
+
+}
